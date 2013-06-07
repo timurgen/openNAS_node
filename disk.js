c249eda545674b48e6c60358795d7a8ebbbd7094
timurgen <timur.samkharadze@gmail.com>
2013-06-07 19:47:37 +0200

    disklayout function testing

--- disk.js
+++ disk.js
@@ -7,25 +7,31 @@ var logger = log4js.getLogger();
 exports.diskinfo = function (callback) {
 	var disklist = new Array();
 	exec('diskinfo -H -p', function (error, stdout, stderr) {
-		if (error != null) {
-			logger.error(error.message);
-		}
-		else {
-			var resultString = stdout.toString();
-			var resultArray = resultString.split(/(\r?\n)/g);
-			for(var i = 0; i < resultArray.length; i++) {
-				var disk;
-				var line = resultArray[i].split('\t');
-				disk.type = line[0];
-				disk.name = line[1];
-				disk.vid = line[2];
-				disk.pid = line[3];
-				disk.size = line[4];
-				disk.remv = line[5];
-				disk.ssd = line[6];
-				disklist[i] = disk;
+			if (error != null) {
+				logger.error(error.message);
 			}
-			callback(disklist);
+			else {
+				var resultString = stdout.toString();
+				var resultArray = resultString.split(/(\r?\n)/g);
+				for (var i = 0; i < resultArray.length; i++) {
+					var line = resultArray[i].split('\t');
+					var disk = {
+							type: line[0],
+							name: line[1],
+							vid: line[2],
+							pid: line[3],
+							size: line[4],
+							remv: line[5],
+							ssd: line[6]
+						}
+						;
+
+					disklist[i] = disk;
+				}
+				callback(disklist);
+			}
 		}
-	});
-};
+	)
+	;
+}
+;
